81d532eef3dc1be0de5779c17f6f80b568d8bc88
CelinexPutte <112859814+CelinexPutte@users.noreply.github.com>
2023-02-08 11:54:54 +0100

    Werken met API en JSON

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Importeer express uit de node_modules map
 import express from 'express'
 
+// Gegevens ophalen
+const url = 'https://whois.fdnd.nl/api/v1/member/celine-van-der-putte'
+const data = await fetch(url).then((response) => response.json())
+
+// console.log(data.member.name)
+
 // Maak een nieuwe express app aan
 const app = express()
 
@@ -14,7 +20,7 @@ app.use(express.static('public'))
 // Maak een route voor de index
 app.get('/', function (req, res) {
   // res.send('Hello World!')
-  res.render('index')
+  res.render('index', data)
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
