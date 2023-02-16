dc61292d8c4c8f018c92e7759e1a61ba279251b6
CelinexPutte <112859814+CelinexPutte@users.noreply.github.com>
2023-02-16 20:54:59 +0100

    Laatste aanpassingen

--- public/script.js
+++
@@ -1,6 +0,0 @@
-const businessCard = document.querySelector('.business-card')
-
-businessCard.addEventListener('click', () => {
-    // console.log('clickieee')
-    businessCard.classList.toggle('flip')
-})
